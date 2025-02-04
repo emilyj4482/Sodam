7ac6b77d09ecfc1a426dd23b2077119a170ec2ff
Emily <emilyj4482@gmail.com>
2025-02-04 10:41:26 +0900

    🐝 [Fix] 경고 메시지 유발했던 코드 수정 - Data-[String] 변환 코드

--- Sodam/Sodam/Extension/+Data.swift
+++ Sodam/Sodam/Extension/+Data.swift
@@ -10,6 +10,6 @@ import Foundation
 /// CoreData에 Data 타입으로 저장되어 있는 imagePaths를 [String]으로 변환
 extension Data {
     var toStringArray: [String]? {
-        return try? NSKeyedUnarchiver.unarchivedObject(ofClass: NSArray.self, from: self) as? [String]
+        return try? NSKeyedUnarchiver.unarchivedArrayOfObjects(ofClass: NSString.self, from: self) as? [String]
     }
 }
