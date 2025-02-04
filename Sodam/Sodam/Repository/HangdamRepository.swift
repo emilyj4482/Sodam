4a9149beb590856e01622be201f0b0e9164c2f06
Emily <emilyj4482@gmail.com>
2025-02-04 16:32:47 +0900

    ✏️ [Modify] HangdamRepository 메소드들이 Result 타입을 반환하도록 변경

--- Sodam/Sodam/Repository/HangdamRepository.swift
+++ Sodam/Sodam/Repository/HangdamRepository.swift
@@ -16,34 +16,57 @@ final class HangdamRepository {
     }
     
     /// 현재 키우는 행담이 불러오기
-    func getCurrentHangdam() -> HangdamDTO {
-        guard !coreDataManager.fetchHangdams().isEmpty,
-              let currentHangdam = coreDataManager.fetchHangdams().last,
-              currentHangdam.endDate == nil
-        else {
-            /// context에 저장된 행담이가 없을 경우(첫 접속) 또는 행담이가 기억 30개를 채운 경우 새로운 행담이 생성
-            return createNewHangdam()
-        }
+    func getCurrentHangdam() -> Result<HangdamDTO, DataError> {
+        let fetchResult = coreDataManager.fetchHangdams()
         
-        return currentHangdam.toDTO
+        switch fetchResult {
+        case .success(let hangdamEntities):
+            guard !hangdamEntities.isEmpty,
+                  let currentHangdam = hangdamEntities.last,
+                  currentHangdam.endDate == nil
+            else {
+                /// context에 저장된 행담이가 없을 경우(첫 접속) 또는 행담이가 기억 30개를 채운 경우 새로운 행담이 생성
+                return createNewHangdam()
+            }
+            return .success(currentHangdam.toDTO)
+        case .failure(let error):
+            return .failure(error)
+        }
     }
     
     /// 보관된 행담이들 불러오기 : 현재 키우는 행담이 제외하고 다 큰 행담이들
-    func getSavedHangdams() -> [HangdamDTO] {
-        guard coreDataManager.fetchHangdams().count > 1 else { return [] }
+    func getSavedHangdams() -> Result<[HangdamDTO], DataError> {
+        let fetchResult = coreDataManager.fetchHangdams()
         
-        return coreDataManager.fetchHangdams().dropLast().compactMap { $0.toDTO }
+        switch fetchResult {
+        case .success(let hangdamEntites):
+            guard hangdamEntites.count > 1 else {
+                return .success([])
+            }
+            return .success(hangdamEntites.dropLast().compactMap{ $0.toDTO })
+        case .failure(let error):
+            return .failure(error)
+        }
     }
     
     /// 새로운 행담이 생성
-    private func createNewHangdam() -> HangdamDTO {
-        return coreDataManager.createHangdam().toDTO
+    private func createNewHangdam() -> Result<HangdamDTO, DataError> {
+        let createResult = coreDataManager.createHangdam()
+        
+        switch createResult {
+        case .success(let entity):
+            return .success(entity.toDTO)
+        case .failure(let error):
+            return .failure(error)
+        }
     }
     
     /// 행담이 이름 짓기
-    func nameHangdam(id: String, name: String) {
-        guard let id = IDConverter.toNSManagedObjectID(from: id, in: coreDataManager.context) else { return }
+    func nameHangdam(id: String, name: String) -> Result<Void, DataError> {
+        guard let id = IDConverter.toNSManagedObjectID(from: id, in: coreDataManager.context) else {
+            return .failure(DataError.convertIDFailed)
+        }
         
-        coreDataManager.updateHangdam(with: id, updateCase: .name(name))
+        return coreDataManager.updateHangdam(with: id, updateCase: .name(name))
     }
 }
